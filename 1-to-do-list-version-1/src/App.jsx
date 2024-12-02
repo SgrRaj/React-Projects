aad79e094a5cc8d0294583fa9564fef0309acbb8
Sagar Raj <sgrraj96@gmail.com>
2024-12-02 20:36:54 +0530

    all

--- 1-to-do-list-version-1/src/App.jsx
+++ 1-to-do-list-version-1/src/App.jsx
@@ -4,14 +4,14 @@ import TodoItem1 from "./TodoItem1";
 import TodoItem2 from "./TodoItem2";
 import './App.css';
 
-// sagar
+
 function App() {
   return (
     <center class="todo-container">
     <AppName></AppName>
     <AppToDo></AppToDo>
     <div class='todoitem-container'>
-    <TodoItem1/>
+    <TodoItem1></TodoItem1>
     <TodoItem2/>
     </div>
     </center>
