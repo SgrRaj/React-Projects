3c618a9df93c5be2805c5b8c2a29c1511eec9bd8
Sagar Raj <sgrraj96@gmail.com>
2024-12-03 12:03:21 +0530

    add new frag

---
+++ 18 Fragments/learning-fragments/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'React'
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+
+function App() {
+   
+  return (
+    <React.Fragment>
+    <ul class="list-group">
+    <li class="list-group-item">Dal</li>
+    <li class="list-group-item">green vegetable</li>
+    <li class="list-group-item">Roti</li>
+    <li class="list-group-item">Salad</li>
+    <li class="list-group-item">Milk</li>
+  </ul>
+  </React.Fragment>
+  );
+}
+
+export default App;
